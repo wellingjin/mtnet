ff78a34a537e183af76e561430976f96ac906465
KingHacker <1096352960@qq.com>
2016-03-22 16:14:59 +0800

    add adapter

---
+++ app/src/main/java/com/welling/kinghacker/customView/ListAdapter.java
@@ -0,0 +1,30 @@
+package com.welling.kinghacker.customView;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by KingHacker on 3/22/2016.
+ **/
+public class ListAdapter extends BaseAdapter{
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
